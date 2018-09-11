cca0db3564f0bda8c2c8d8e347f2366d4a31f312
RyanAveo <ryan.aveo@gmail.com>
2018-09-11 11:09:39 -0700

    Added functionality to choose a random color from colors array. Incorrect squares will go away. Once correct color is chosen, all squares will be that color

--- colorGame.js
+++ colorGame.js
@@ -8,10 +8,44 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
-var colorDisplay = document.getElementsById("colorDisplay");
+// pick a random color from colors
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
+
 colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++) {
+	//add initial colors to squares
 	squares[i].style.backgroundColor = colors[i];
+
+	// add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked square
+		var clickedColor = this.style.backgroundColor;
+
+		//compare clicked color to picked color
+		if (clickedColor === pickedColor){
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
+		}
+		else {
+			//document.body.style represents inline styles in the body NOT the style given in colorGame.css
+			var bodyStyle = window.getComputedStyle(document.body);
+			this.style.backgroundColor = bodyStyle.getPropertyValue('background-color');
+			messageDisplay.textContent = "Try Again"
+		}
+	});
+}
+
+//change all squares backgroundColor to color
+function changeColors(color) {
+	// loop through all squares
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.backgroundColor = color;
+	}
+}
+//return a random color
+function pickColor() {
+	var index = Math.floor(Math.random() * colors.length);
+	return colors[index];
 }
